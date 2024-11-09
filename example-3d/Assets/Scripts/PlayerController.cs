75cbaeedc957cc9e01534906f72eba0150fea4ad
igavriel <igavriel@gmail.com>
2024-11-10 00:01:45 +0200

    PlayerController

---
+++ example-3d/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed { get; set; }
+
+    private Rigidbody rb;
+    private float xInput, yInput;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
